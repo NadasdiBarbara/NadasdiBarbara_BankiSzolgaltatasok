35d918ea9d2849d3395b4ce5f1ee23fd65e6c62c
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2021-11-01 16:31:04 +0100

    MegtakaritasiSzamla osztály leszármazás, kamat, alapkamat, Kivesz(), Kamatjovairas()

--- src/hu/petrik/MegtakaritasiSzamla.java
+++ src/hu/petrik/MegtakaritasiSzamla.java
@@ -1,4 +1,33 @@
 package hu.petrik;
 
-public class MegtakaritasiSzamla {
+public class MegtakaritasiSzamla extends Szamla {
+    private double kamat;
+    private static double alapkamat = 1.8;
+
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        this.kamat = alapkamat;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    @Override
+    public boolean kivesz(double osszeg) {
+        boolean siker = true;
+        if (osszeg > getAktualisEgyenleg()){
+            siker = false;
+        }else{
+            setAktualisEgyenleg(getAktualisEgyenleg() - osszeg);
+        }
+        return siker;
+    }
+    public void kamatjovairas(){
+        setAktualisEgyenleg(getAktualisEgyenleg()*kamat);
+    }
 }
